d4de78c6f9620100aa04bfb658efc00bb2e77ce1
Chan Kang <me@kentakang.com>
2021-08-12 14:54:41 +0900

    feat: add useWindowSize hooks

--- src/pages/world/index.tsx
+++ src/pages/world/index.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Stage } from '@inlet/react-pixi';
 
-const World = () => (
-  <>
-    <Stage />
-  </>
-);
+import useWindowSize from '../../hooks/useWindowSize';
+
+const World = () => {
+  const windowSize = useWindowSize();
+  const [stageWidth, setStageWidth] = useState<number | undefined>(0);
+  const [stageHeight, setStageHeight] = useState<number | undefined>(0);
+
+  useEffect(() => {
+    setStageWidth(windowSize.width);
+    setStageHeight(windowSize.height);
+  }, [windowSize]);
+
+  return (
+    <>
+      <Stage width={stageWidth} height={stageHeight} />
+    </>
+  );
+};
 
 export default World;
